2bac15f8753ad745c2b061d9110f623634ddb444
Goodvick <teplicov23@gmail.com>
2025-09-04 11:40:27 +0300

    vue_sneakers_v1.0

--- src/components/DrawerSneakers.vue
+++ src/components/DrawerSneakers.vue
@@ -14,9 +14,9 @@ defineProps({
   <div
     class="fixed bg-zinc-200 w-3/4 sm:w-5/10 lg:w-3/8 xl:w-2/6 h-9/10 sm:h-full rounded-b-2xl sm:rounded-b-[0px] shadow-[0_0_10px_0.2px_white] top-0 left-[50%] translate-x-[-50%] sm:translate-x-0 sm:left-0 z-11 p-2 sm:p-4 duration-600">
     <div class="flex justify-center sm:justify-between items-center">
-      <h1
+      <h2
         class="text-[15px] sm:text-[13px] md:text-[16px] lg:text-[20px] xl:text-[28px] text-center sm:text-start font-semibold duration-600">
-        Корзина</h1>
+        Корзина</h2>
       <div @click="$emit('closeCart')" class="h-[25px] pt-2">
         <svg
           class="-rotate-90 sm:rotate-180 sm:opacity-60 sm:hover:opacity-100 ml-4 sm:ml-0 cursor-pointer hover:-translate-x-1 duration-300"
